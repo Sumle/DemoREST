f642b7e5a5820f77e67cee2816178e7ca31ff937
Siluu <silk0293@edu.zealand.dk>
2023-03-20 13:20:02 +0100

    api/controller

--- DemoREST/Controllers/TeacherController.cs
+++ DemoREST/Controllers/TeacherController.cs
@@ -4,7 +4,7 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace DemoREST.Controllers
 {
-    [Route("api/[controller")]
+    [Route("api/[controller]")]
     [ApiController]
     public class TeacherController : ControllerBase
     {
